c818a794b165c8f5469222b7e6b49f7c8918671e
Sasha Lohozinskyy <0x0sky@proton.me>
2026-07-31 19:25:18 +0300

    Enforce status card handler ownership

--- test/handler_architecture_test.rb
+++ test/handler_architecture_test.rb
@@ -6,8 +6,10 @@ require "zero_x_da/market_client_bot/bot"
 class HandlerArchitectureTest < Minitest::Test
   Bot = ZeroXDA::MarketClientBot::Bot
   AdminMessages = ZeroXDA::MarketClientBot::AdminMessages
+  StatusCards = ZeroXDA::MarketClientBot::StatusCards
   ADMIN_HANDLERS = %i[
     show_servers
+    refresh_servers
     show_active_users
     start_price_application
     show_fx_rates
@@ -19,6 +21,10 @@ class HandlerArchitectureTest < Minitest::Test
       assert_equal AdminMessages, Bot.instance_method(handler).owner, handler
       refute_includes Bot.private_instance_methods(false), handler
     end
+  end
+
+  def test_status_callback_dispatch_has_one_canonical_owner
+    assert_equal StatusCards, Bot.instance_method(:handle_callback).owner
   end
 
   def test_legacy_shadowing_modules_are_not_loaded
