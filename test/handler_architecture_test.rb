abd935d4abe2500439eb399acc2457ac30b9a474
Sasha Lohozinskyy <0x0sky@proton.me>
2026-07-31 17:04:23 +0300

    test: enforce canonical handler ownership

---
+++ test/handler_architecture_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+require "zero_x_da/market_client_bot/bot"
+
+class HandlerArchitectureTest < Minitest::Test
+  Bot = ZeroXDA::MarketClientBot::Bot
+  AdminMessages = ZeroXDA::MarketClientBot::AdminMessages
+  ADMIN_HANDLERS = %i[
+    show_servers
+    show_active_users
+    start_price_application
+    show_fx_rates
+    set_admin
+  ].freeze
+
+  def test_administrator_handlers_have_one_canonical_owner
+    ADMIN_HANDLERS.each do |handler|
+      assert_equal AdminMessages, Bot.instance_method(handler).owner, handler
+      refute_includes Bot.private_instance_methods(false), handler
+    end
+  end
+
+  def test_legacy_shadowing_modules_are_not_loaded
+    refute ZeroXDA::MarketClientBot.const_defined?(:TelegramUserLinks, false)
+    refute ZeroXDA::MarketClientBot.const_defined?(:LocalizedLegacyCopy, false)
+    refute ZeroXDA::MarketClientBot.const_defined?(:LocalizedServerStartNotice, false)
+  end
+end
