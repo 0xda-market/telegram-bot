de2ed7ebb8e29e629460f28e0fe216184bcfcec2
Sasha Lohozinskyy <0x0sky@proton.me>
2026-08-01 03:35:29 +0300

    test: cover persistent Telegram runtime

---
+++ test/persistent_runtime_test.rb
@@ -0,0 +1,45 @@
+require_relative "test_helper"
+require "zero_x_da/market_client_bot/bot"
+require "zero_x_da/market_client_bot/persistent_runtime"
+
+class PersistentRuntimeTest < Minitest::Test
+  def test_slow_command_does_not_claim_that_the_persistent_server_is_starting
+    market = Class.new(FakeMarketAPI) do
+      def authenticate_telegram(**arguments)
+        sleep 0.03
+        super
+      end
+    end.new
+    telegram = FakeTelegramAPI.new
+    bot = ZeroXDA::MarketClientBot::Bot.new(
+      market_api: market,
+      telegram_api: telegram,
+      server_start_notice_delay: 0.005,
+      status_message_ttl: 0
+    )
+    bot.extend(ZeroXDA::MarketClientBot::PersistentRuntime)
+
+    bot.handle(update("/start"))
+
+    refute telegram.messages.any? { |message| message.fetch(:text) == "Сервер запускається…" }
+    assert_includes telegram.messages.last.fetch(:text), "авторизація успішна"
+  end
+
+  private
+
+  def update(text)
+    {
+      "message" => {
+        "message_id" => 10,
+        "text" => text,
+        "from" => {
+          "id" => 77,
+          "username" => "zero",
+          "first_name" => "Sasha",
+          "language_code" => "uk"
+        },
+        "chat" => { "id" => 770, "type" => "private" }
+      }
+    }
+  end
+end
